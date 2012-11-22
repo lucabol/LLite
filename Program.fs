410c713f7071621771cee59d4311c985da41b065
Luca Bolognese <lucabolg@gmail.com>
2012-11-22 09:50:10 +0000

    First commit

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿let fp = ref (Unchecked.defaultof<float -> float>)
+
+let fwdDecl<'a> () = ref Unchecked.defaultof<'a>
+
+let takeTurns = fwdDecl<float -> float> ()
+
+takeTurns := fun x -> x + 1.
+
+takeTurns := fun x -> x + 3.
+
+!takeTurns 3.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
